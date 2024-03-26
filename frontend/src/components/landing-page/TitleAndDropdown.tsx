901359d539af4566a5db35b8fd11a78597f04644
sentig <156699725+sentig@users.noreply.github.com>
2024-03-25 17:25:18 -0700

    Update TitleAndDropdown.tsx

--- frontend/src/components/landing-page/TitleAndDropdown.tsx
+++ frontend/src/components/landing-page/TitleAndDropdown.tsx
@@ -72,40 +72,21 @@ export const TitleAndDropdown = () => {
 
   return (
     <div className="landing-page-gradient-1 relative flex h-max w-screen flex-col items-center font-lora ">
-      <div className="absolute right-4 top-4">
-        <a href="https://www.llamaindex.ai/" target="_blank">
-          <button className="flex items-center justify-center font-nunito text-lg font-bold ">
-            Built by LlamaIndex
-            <img src="logo-black.svg" className="mx-2 rounded-lg" width={40} />
-          </button>
-        </a>
-      </div>
       <div className="mt-28 flex flex-col items-center">
         <div className="w-4/5 text-center text-4xl">
-          Empower your organization&apos;s Business Intelligence with{" "}
-          <span className="font-bold">SEC Insights </span>
-        </div>
-        <div className="mt-4 flex items-center justify-center">
-          <div className="w-3/5 text-center font-nunito">
-            Effortlessly analyze multifaceted financial documents such as 10-Ks
-            and 10-Qs.
-          </div>
-        </div>
-        <div className="mt-4 flex items-center justify-center">
-          <GitHubButton href="https://github.com/run-llama/sec-insights">Open-Sourced on Github</GitHubButton>
+          sentipact
         </div>
       </div>
       {isMobile ? (
         <div className="mt-12 flex h-1/5 w-11/12 rounded border p-4 text-center">
           <div className="text-xl font-bold">
-            To start analyzing documents, please switch to a larger screen!
+            mobile not supported, please switch to desktop
           </div>
         </div>
       ) : (
         <div className="mt-5 flex h-min w-11/12 max-w-[1200px] flex-col items-center justify-center rounded-lg border-2 bg-white sm:h-[400px] md:w-9/12 ">
           <div className="p-4 text-center text-xl font-bold">
-            Start your conversation by selecting the documents you want to
-            explore
+            select your documents
           </div>
           <div className="h-1/8 flex w-full flex-wrap items-center justify-center font-nunito">
             <div className="m-1 flex w-96 items-center">
@@ -189,7 +170,7 @@ export const TitleAndDropdown = () => {
                   <CgFileDocument size={46} />
                 </div>
                 <div className="w-84 text-center md:w-64">
-                  Use the document selector above to start adding documents
+                  selected documents will display here
                 </div>
               </div>
             )}
