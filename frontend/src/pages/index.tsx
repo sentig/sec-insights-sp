e8d286111e6b67662cfeff8549b3c7bcb56b6258
sentig <156699725+sentig@users.noreply.github.com>
2024-03-25 17:27:24 -0700

    Update index.tsx

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import type { NextPage } from "next";
-import { MarketingSection } from "~/components/landing-page/MarketingSection";
 import { TitleAndDropdown } from "~/components/landing-page/TitleAndDropdown";
 
 const LandingPage: NextPage = () => {
